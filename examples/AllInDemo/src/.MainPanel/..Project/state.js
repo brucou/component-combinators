1d85d020092b7555856e2100ab781c67f8afd279
brucou <bruno.couriol@gmail.com>
2018-01-03 02:59:15 +0100

    feat: implemented tabbar of main panel

---
+++ examples/AllInDemo/src/.MainPanel/..Project/state.js
@@ -0,0 +1,24 @@
+import * as Rx from "rx";
+import { keys, values } from 'ramda'
+import { ROUTE_PARAMS } from "../../../../../src/components/Router/properties"
+
+const $ = Rx.Observable;
+
+export function projectsStateFactory(sources, settings) {
+  const { [ROUTE_PARAMS]: { projectId } } = settings;
+
+  return sources.projectsFb$
+    .map(projectsFb => {
+      const fbKeys = keys(projectsFb);
+      const _values = values(projectsFb);
+      const index = _values.findIndex(project => project._id === projectId);
+      const fbIndex = fbKeys[index];
+      const project = _values[index];
+
+      return {
+        fbIndex,
+        project
+      }
+    })
+    .shareReplay(1)
+}
--- examples/AllInDemo/src/.MainPanel/index.js
+++ examples/AllInDemo/src/.MainPanel/index.js
@@ -1,3 +1,13 @@
-import { DummyComponent } from "../../../../src"
+import { OnRoute } from "../../../../src/components/Router/Router"
+import { Div } from "../../../../src/utils"
+import { m } from "../../../../src/components/m/m"
+import { Project } from './..Project'
+import { ProjectsDashboard } from './..ProjectsDashboard'
+import { ManagePlugins } from './..ManagePlugins'
 
-export const MainPanel = DummyComponent;
+export const MainPanel =
+  m({}, {}, [Div(`.app__l-main`), [
+    OnRoute({ route: 'dashboard' }, [ProjectsDashboard]),
+    OnRoute({ route: 'projects/:projectId' }, [Project]),
+    OnRoute({ route: 'plugins' }, [ManagePlugins]),
+  ]]);
